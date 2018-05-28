896ee415a6a7cdb25ff74d979760f262ded949cc
David Willie <david.willie.1@gmail.com>
2018-05-28 10:44:56 +1000

    Handle partial reloads, handle the PR list (badly)

--- background.js
+++ background.js
@@ -3,7 +3,18 @@ var storageItem = browser.storage.sync.get('orgName');
 console.log(storageItem);
 
 storageItem.then(a => { 
-    prTitle = document.getElementsByClassName("js-issue-title")[0];
     const ticketTagRegex = /([A-Z]+\-[0-9]+)+/g;
-    prTitle.innerHTML = prTitle.innerHTML.replace(ticketTagRegex, "<a href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">$&</a>"); 
+
+    setInterval(() => {
+        var prTitle = document.getElementsByClassName("js-issue-title")[0];
+        if (prTitle && prTitle.innerHTML.indexOf("atlassian.net") == -1) { // Already contains a link to JIRA
+            prTitle.innerHTML = prTitle.innerHTML.replace(ticketTagRegex, "<a target=\"_blank\" href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">$&</a>"); 
+        }
+
+        document.querySelectorAll(".p-0").forEach(element => {
+            if (element.innerHTML.indexOf("atlassian.net") == -1) {
+                element.innerHTML = element.innerHTML.replace(ticketTagRegex, "</a><a style=\"font-weight:bold\" target=\"_blank\" href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">$&</a>"); 
+            }
+        });
+    }, 1000);
 });
