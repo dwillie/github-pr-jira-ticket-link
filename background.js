03e8c01021cc274b0e92f23e98db5a114dcffec0
David Willie <david.willie.1@gmail.com>
2018-05-31 13:19:00 +1000

    Fix PR review bug and improve styling on PR list

--- background.js
+++ background.js
@@ -7,14 +7,25 @@ storageItem.then(a => {
 
     setInterval(() => {
         var prTitle = document.getElementsByClassName("js-issue-title")[0];
-        if (prTitle && prTitle.innerHTML.indexOf("atlassian.net") == -1) { // Already contains a link to JIRA
+        if (unlinked(prTitle)) { // Already contains a link to JIRA
             prTitle.innerHTML = prTitle.innerHTML.replace(ticketTagRegex, "<a target=\"_blank\" href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">$&</a>"); 
         }
 
-        document.querySelectorAll(".p-0").forEach(element => {
-            if (element.innerHTML.indexOf("atlassian.net") == -1) {
-                element.innerHTML = element.innerHTML.replace(ticketTagRegex, "</a><a style=\"font-weight:bold\" target=\"_blank\" href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">$&</a>"); 
+        document.querySelectorAll("div.col-9").forEach(element => {
+            if (unlinked(element)) {
+                element.innerHTML = element.innerHTML.replace(ticketTagRegex, "\
+                    <a \
+                    class=\"v-align-middle no-underline h4 js-navigation-open\" \
+                    target=\"_blank\" \
+                    href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">\
+                        $&\
+                    </a>\
+                    <a href=\"" + element.firstElementChild.getAttribute("href") + "\" class=\"link-gray-dark v-align-middle no-underline h4 js-navigation-open\">"); 
             }
         });
     }, 1000);
 });
+
+function unlinked(element) {
+    return element && element.innerHTML.indexOf("atlassian.net") == -1;
+}
