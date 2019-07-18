cb2a2d0cb2022c8d8658ab826fdc9290f0a91bed
dwillie <david.willie.1@gmail.com>
2019-07-18 15:59:49 +1000

    Fix bug with storage apis (old way?) and improve handling of page load and refreshing

--- background.js
+++ background.js
@@ -1,31 +1,55 @@
-console.log(browser.storage.managed);
-var storageItem = browser.storage.sync.get('orgName');
-console.log(storageItem);
-
-storageItem.then(a => { 
+const storageItem = chrome.storage.local.get('orgName', userData => {
     const ticketTagRegex = /([A-Z]+\-[0-9]+)+/g;
 
-    setInterval(() => {
-        var prTitle = document.getElementsByClassName("js-issue-title")[0];
-        if (unlinked(prTitle)) { // Already contains a link to JIRA
-            prTitle.innerHTML = prTitle.innerHTML.replace(ticketTagRegex, "<a target=\"_blank\" href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">$&</a>"); 
+    var lastHandledLocation = "";
+
+    const intervalId = setInterval(() => {
+        if (lastHandledLocation === document.location.href) {
+            console.log("Location not changed: " + lastHandledLocation + " vs " + document.location.href);
+            return;
         }
 
-        document.querySelectorAll("div.col-9").forEach(element => {
-            if (unlinked(element)) {
-                element.innerHTML = element.innerHTML.replace(ticketTagRegex, "\
-                    <a \
-                    class=\"v-align-middle no-underline h4 js-navigation-open\" \
-                    target=\"_blank\" \
-                    href=\"http://" + a.orgName + ".atlassian.net/browse/$&\">\
-                        $&\
-                    </a>\
-                    <a href=\"" + element.firstElementChild.getAttribute("href") + "\" class=\"link-gray-dark v-align-middle no-underline h4 js-navigation-open\">"); 
+        console.log("Location changed: " + document.location.href + " Allowing the page to load before attempting linking.");
+        setTimeout(() => {
+            const prTitle = document.querySelector("span.js-issue-title");
+            if (prTitle != null && isUnlinked(prTitle)) {
+                prTitle.innerHTML = prTitle.innerHTML.replace(ticketTagRegex, prTitleLink(userData.orgName));
+
+                // PR context confirmed, linking handled, set lastHandledLocation to prevent further refreshes
+                lastHandledLocation = document.location.href;
+                return;
             }
-        });
+
+            const prLinks = document.querySelectorAll("a.h4");
+            prLinks.forEach(element => {
+                if (element.id.match(/issue_[0-9]+/) != null && isUnlinked(element)) {
+                    element.outerHTML = element.outerHTML.replace(ticketTagRegex, prLinkLink(userData.orgName, element));
+                }
+            });
+
+            if (prLinks.length > 0) {
+                // PR List context confirmed, linking handled, set lastHandledLocation to prevent further refreshes
+                lastHandledLocation = document.location.href;
+            }
+        }, 2500);
     }, 1000);
 });
 
-function unlinked(element) {
+function isUnlinked(element) {
     return element && element.innerHTML.indexOf("atlassian.net") == -1;
+}
+
+function prTitleLink(orgName) {
+    return "<a id=\"\" target=\"_blank\" href=\"http://" + orgName + ".atlassian.net/browse/$&\">$&</a>";
+}
+
+function prLinkLink(orgName, element) {
+    return "\
+    </a><a \
+    class=\"v-align-middle no-underline h4 js-navigation-open\" \
+    target=\"_blank\" \
+    href=\"http://" + orgName + ".atlassian.net/browse/$&\">\
+        $&\
+    </a>\
+    <a href=\"" + element.getAttribute("href") + "\" class=\"link-gray-dark v-align-middle no-underline h4 js-navigation-open\">";
 }
--- options.js
+++ options.js
@@ -1,18 +1,13 @@
 function saveOptions(e) {
-  browser.storage.sync.set({
+  chrome.storage.local.set({
     orgName: document.querySelector("#orgName").value
   });
   e.preventDefault();
 }
 
 function restoreOptions() {
-  var storageItem = browser.storage.managed.get('orgName');
-  storageItem.then((res) => {
+  chrome.storage.local.get('orgName', (res) => {
     document.querySelector("#orgName").innerText = res.orgName;
-  });
-
-  var gettingItem = browser.storage.sync.get('orgName');
-  gettingItem.then((res) => {
     document.querySelector("#orgName").value = res.orgName || 'Pick an Org Name';
   });
 }
